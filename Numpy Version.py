358ab2c25d41a583a5f5b1404930a777689697dc
Barakat273 <es-Abdelrahman.Hassan2026@alexu.edu.eg>
2025-02-28 16:00:48 +0200

    Add files via upload

---
+++ Numpy Version.py
@@ -0,0 +1,56 @@
+import numpy as np # ( Modified )
+
+def slab_transmission(Sig_s, Sig_a, thickness, N):
+    Sig_t = Sig_a + Sig_s
+    iSig_t = 1 / Sig_t  # Mean free path
+    transmission = 0.0
+    N = int(N)
+    tot_colls = 0 # ( Modified )
+
+    for i in range(N):
+        # For a beam, the initial direction is fixed (mu = 1)
+        mu = 1.0 # cos of the angle of the neutron
+        x = 0.0
+        alive = True
+        colls = 0 # For the number of collisions for the neutron (Modified)
+
+        while alive:
+            # Sample distance to the next collision
+            l = -np.log(1 - np.random.random()) * iSig_t
+            x += l * mu  # Move particle along the x-direction
+
+            # Check if the neutron has left the slab
+            if x > thickness:
+                transmission += 1
+                alive = False
+            elif x < 0:
+                alive = False
+            else:
+                # Determine if the neutron scatters or gets absorbed
+                if np.random.random() < Sig_s * iSig_t:
+                    # Scattering event: choose a new direction (still isotropic scattering)
+                    mu = np.random.uniform(-1, 1)
+                    # Count the number of collisions for scattering (Modified)
+                    colls += 1
+                else:
+                    # Absorption event: terminate the neutron's history
+                    colls += 1 # Count the number of collisions for absorption (Modified)
+                    alive = False
+
+        # Add the number of collisions for this neutron to the total
+        tot_colls += colls # Modified
+
+    # Calculate the average number of collisions (Modified)
+    avg_colls = tot_colls / N
+    return transmission / N, avg_colls # ( Modified )
+
+# Example usage:
+N = 100000
+Sigma_s = 0.75
+Sigma_a = 2.0 - Sigma_s
+thickness = 3.0
+transmission, avg_colls = slab_transmission(Sigma_s, Sigma_a, thickness, N) # ( Modified )
+
+print("Out of", N, "neutrons, only", int(transmission * N),
+      "made it through.\nThe fraction that made it through was", transmission)
+print("Average collisions per neutron:", avg_colls) # ( Modified )
